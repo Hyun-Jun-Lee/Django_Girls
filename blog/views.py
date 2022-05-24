458c9d824c555c240c821f77c17f8562732b8a2e
logstack7 <logstack@logstackui-iMac-5.local>
2022-05-24 17:18:47 +0900

    complete

--- blog/views.py
+++ blog/views.py
@@ -70,10 +70,12 @@ def add_comment_to_post(request, pk):
 
 def comment_approve(request,pk):
     comment = get_object_or_404(Comment, pk=pk)
+    print(comment)
     comment.approve()
-    return redirect('post_detail', pk=comment.post.pk)
+    return redirect('post_list')
 
 def comment_remove(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
-    comment.delte()
+    print(comment)
+    comment.delete()
     return redirect('post_detail',pk=comment.post.pk)
