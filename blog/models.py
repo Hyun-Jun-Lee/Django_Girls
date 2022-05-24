c4c1b2d37694bbb8d3af44ab08d318fcc2ed3a7e
logstack7 <logstack@logstackui-iMac-5.local>
2022-05-24 15:42:34 +0900

    Django girls Tutorial

---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+# Create your models here.
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_at = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=150)
+    text = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+
